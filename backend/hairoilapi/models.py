bc5db9260f04489e05e883e6864133bc7f427fbd
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2023-03-11 00:00:07 +0530

    models done!

--- backend/hairoilapi/models.py
+++ backend/hairoilapi/models.py
@@ -1,3 +1,39 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class Userregister(models.Model):
+    username=models.CharField(max_length=100)
+    email=models.EmailField()
+    phonenumber=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    registertime=models.TimeField(auto_now_add=True)
+    
+class Usercoupon(models.Model):
+    userid=models.ForeignKey(Userregister,on_delete=models.CASCADE)
+    no_of_coupon=models.IntegerField(default=0)
+    reference=models.CharField(max_length=100)
+    
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(Userregister, on_delete=models.CASCADE)
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s customer profile"
+    
+class Productpurchase(models.Model):
+    userid=models.ForeignKey(Userregister,on_delete=models.CASCADE)
+    productprice=models.CharField(max_length=100)
+    productname=models.CharField(max_length=100)
+    purchasetime=models.TimeField(auto_now_add=True)
+    
+class Delivery(models.Model):
+    prodid=models.ForeignKey(Productpurchase,on_delete=models.CASCADE)
+    city=models.CharField(max_length=100)
+    state=models.CharField(max_length=100)
+    address=models.CharField(max_length=1000)
+    landmark=models.CharField(max_length=100)
+    pincode=models.CharField(max_length=100)
+    
+    
